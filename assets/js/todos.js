686ae9097bf1eb64014237bd73c59d181daa1ae0
Cameron <cameron.barber.94@gmail.com>
2017-02-01 02:26:45 +1100

    Can now add items to the list and delete/line-through new items
    Changed the target for the click listener to the “ul” then target the
    “lI” within the “ul” element so it is possible to delete/line-through
    the dynamically made elements.
    Also found out jQuery slim doesn’t include any of the animation
    effects, wish they’d put that on the front of the download page!

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,22 @@
 // check off list items by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
+});
+
+//deleting the todo list by clicking on X
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove;
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13){
+    //taking new list items from the text input
+    var todoText = $(this).val();
+    $(this).val("");
+    //creating the new li and adding to the parent ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
