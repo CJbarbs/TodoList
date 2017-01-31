cfc1968c4c3f15cd6780e4555a240a045690ab14
Cameron <cameron.barber.94@gmail.com>
2017-02-01 01:09:36 +1100

    initial Commit
    Making a todo list, with initial jQuery code to change the colour of
    the list item when clicked from black to gray and line-through and when
    clicked again it will return to initial colour of black.

---
+++ assets/js/todos.js
@@ -0,0 +1,14 @@
+// check off list items by clicking
+$("li").click(function() {
+  if ($(this).css("color") === "rgb(128, 128, 128)"){
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  } else {
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
+});
